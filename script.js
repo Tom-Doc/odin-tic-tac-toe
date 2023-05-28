de38c5621e542b21ca67030e1566e264926e06ff
Tom <Tomdocherty1@gmail.com>
2023-05-28 11:43:56 -0700

    feat: Implement  modal functionality for entering players name feat: Add event listeners for player name display

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ const startButton = document.querySelector(".start-game-btn");
 const submitNameBtn = document.querySelector(".submit-name-btn");
 const closeModalBtn = document.querySelector(".close-modal-btn");
 
+//Array to store player names
+let playerNames = [];
+let player1Name = document.getElementById("name1");
+let player2Name = document.getElementById("name2");
+playerNames.push(player1Name);
+playerNames.push(player2Name);
+
+function displayPlayerNames() {
+  const player1NameDisplay = document.querySelector(".player1-name-display");
+  const player2NameDisplay = document.querySelector(".player2-name-display");
+
+  player1NameDisplay.textContent = playerNames[0].value;
+  player2NameDisplay.textContent = playerNames[1].value;
+}
+
+submitNameBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  displayPlayerNames();
+  modal.close();
+});
+
 // Shows modal
 startButton.addEventListener("click", () => {
   modal.showModal();

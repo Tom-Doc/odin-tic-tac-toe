71c64f3db81bbd0f05013a5e0a658d6ba81600b2
Tom <Tomdocherty1@gmail.com>
2023-05-31 18:45:10 -0700

    Add gameBoard module Add initializeBoard function Add getBoard function

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let player2Name = document.getElementById("name2");
 playerNames.push(player1Name);
 playerNames.push(player2Name);
 
+//Function retrieve and add player names to DOM
 function displayPlayerNames() {
   const player1NameDisplay = document.querySelector(".player1-name-display");
   const player2NameDisplay = document.querySelector(".player2-name-display");
@@ -19,17 +20,45 @@ function displayPlayerNames() {
   player2NameDisplay.textContent = playerNames[1].value;
 }
 
+//Submit button to add names to DOM
 submitNameBtn.addEventListener("click", (event) => {
   event.preventDefault();
   displayPlayerNames();
+  modal.classList.remove("open");
   modal.close();
 });
 
-// Shows modal
+// Shows modal button
 startButton.addEventListener("click", () => {
+  modal.classList.add("open");
   modal.showModal();
 });
 
+//Close modal button
 closeModalBtn.addEventListener("click", (event) => {
+  modal.classList.remove("open");
   modal.close();
 });
+
+//Module to build game board
+const gameBoard = (function () {
+  //Private variables
+  let board = [];
+
+  //Private function to initialize the game board
+  function initializeBoard() {
+    let board = Array(9).fill(""); //Creates an array of 9 empty cells
+  }
+
+  //Public function to get the current game baord
+  function getBoard() {
+    return board;
+  }
+
+  // Other public functions:
+
+  return {
+    initializeBoard,
+    getBoard,
+  };
+})();
